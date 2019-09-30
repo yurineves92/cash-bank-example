8e1e73e7b657a6e09ec73bbba8fe9068bd4c783d
Yuri Neves <yurineves92@gmail.com>
2019-09-30 11:50:14 -0300

    lasted version

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -17,28 +17,17 @@
     <!-- Navigation -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="/">Example Tenancy</a>
+            <a class="navbar-brand" href="/">Cash Bank</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/">Dashboard</a>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link" href="/accounts">Contas</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/accounts/movements">Histórico</a>
-                </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownLogin" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Yuri Neves
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownLogin">
-                    <a class="dropdown-item" href="/logout">Sair</a>
-                </div>
                 </li>
             </ul>
             </div>
